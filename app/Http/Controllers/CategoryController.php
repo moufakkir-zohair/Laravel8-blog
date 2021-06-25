48a78c75f689653be256f4b8c188d41be42ee197
moufakkir-zohair <zohair.moufakkir@usmba.ac.ma>
2021-06-25 14:00:40 +0100

    create_category_controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -43,17 +43,7 @@ class CategoryController extends Controller
         return redirect('/categories');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
+    
     /**
      * Show the form for editing the specified resource.
      *
